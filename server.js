8cc99b02569a64783e10a1e7125fa76ec9ee5a88
generalwellbeing <leighbrodie@gmail.com>
2017-03-31 15:41:23 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var ip = require("ip");
+var os = require("os");
+
+var port = 8080;
+
+var app = express();
+
+app.get('/', function(req, res) {
+    console.log(req);
+    
+    var ipAddress = ip.address();
+    var language = req.headers["accept-language"];
+    var software = os.platform();
+    
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ "ipaddress": ipAddress, "language": language, "software": software}));
+});
+
+app.listen(port, function () {
+    console.log('Request Header Parser Microservice app listening on port ' + port);
+});
